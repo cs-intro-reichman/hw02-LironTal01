c02244d7ab5594e5f56e30baa2946adb0971c437
LironTal01 <154249302+LironTal01@users.noreply.github.com>
2023-12-29 23:13:36 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -10,8 +10,9 @@
 public class OneOfEachStats {
     public static void main(String[] args) {
         // Gets the two command-line arguments
-        int t = Integer.parseInt(args[0]);
-        int seed = Integer.parseInt(args[1]);
+        int seed = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+      
         // Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);
 
@@ -23,7 +24,7 @@ public class OneOfEachStats {
         int sum3 = 0;
         int sum4 = 0;
 
-        for(int i=0; i<t; i++){
+        for(int i=0; i<T; i++){
         while(b==0 || g==0){
             x = generator.nextDouble();
             if (x>0.5) {
